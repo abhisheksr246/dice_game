c8a35ced90e8655fe20227f61785dcf938b45c0f
Abhishek SR <95201220+abhisheksr246@users.noreply.github.com>
2023-06-18 17:10:16 +0530

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,17 @@
+var randomNum1 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img1").setAttribute("src", "images/dice" +randomNum1+ ".png");
+
+var randomNum2 = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector(".img2").setAttribute("src", "images/dice" +randomNum2+ ".png");
+
+if(randomNum1 > randomNum2){
+    document.querySelector("h1").innerHTML = "🚩 Player 1 Wins 🚩";
+}
+else if(randomNum1 < randomNum2){
+    document.querySelector("h1").innerHTML = "🚩 Player 2 Wins 🚩";
+}
+else if(randomNum1 === randomNum2){
+    document.querySelector("h1").innerHTML = "Draw Again!";
+}
